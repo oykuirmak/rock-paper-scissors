fccc27d070c1d3eeed5484c460eb44919407a925
oykuirmak <96113313+oykuirmak@users.noreply.github.com>
2021-12-14 12:46:32 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,38 @@
+let el = 0
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    el = randint(1, 3)
+    if (el == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (el == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
